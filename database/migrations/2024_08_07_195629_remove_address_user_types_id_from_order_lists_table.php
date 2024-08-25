d18217fd401ae2b15d000d8efdbc427098d03b15
alexandre <alexandresousaroberto@gmail.com>
2024-08-25 19:29:37 -0300

    alterações no tipo de tabela que não esta sendo compativel

--- database/migrations/2024_08_07_195629_remove_address_user_types_id_from_order_lists_table.php
+++ database/migrations/2024_08_07_195629_remove_address_user_types_id_from_order_lists_table.php
@@ -9,22 +9,22 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::table('order_lists', function (Blueprint $table) {
-            $table->dropColumn('address_user_types_id');
-        });
-    }
+//     public function up(): void
+//     {
+//         Schema::table('order_lists', function (Blueprint $table) {
+//             $table->dropColumn('address_user_types_id');
+//         });
+//     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('order_lists', function (Blueprint $table) {
-            $table->unsignedBigInteger('address_user_types_id')->nullable();
-        // Adicione uma chave estrangeira se houver:
-        $table->foreign('address_user_types_id')->references('id')->on('address_user_types');
-        });
-    }
-};
+//     /**
+//      * Reverse the migrations.
+//      */
+//     public function down(): void
+//     {
+//         Schema::table('order_lists', function (Blueprint $table) {
+//             $table->unsignedBigInteger('address_user_types_id')->nullable();
+//         // Adicione uma chave estrangeira se houver:
+//         $table->foreign('address_user_types_id')->references('id')->on('address_user_types');
+//         });
+//     }
+// };
