1c054ce623529b678b23a2881c247df158f895a6
alexandre <alexandresousaroberto@gmail.com>
2024-08-26 10:25:50 -0300

    ajustando migrate para rodar em produção2

--- database/migrations/2024_05_01_175949_create_address_user_types_table.php
+++ database/migrations/2024_05_01_175949_create_address_user_types_table.php
@@ -1,14 +1,14 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+// use Illuminate\Database\Migrations\Migration;
+// use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+// return new class extends Migration
+// {
+//     /**
+//      * Run the migrations.
+//      */
 //     public function up(): void
 //     {
 //         Schema::create('address_user_types', function (Blueprint $table) {
