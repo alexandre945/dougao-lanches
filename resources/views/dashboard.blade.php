2a6f901a7356044ca02e87250d08d5da38d1bd70
alexandre <alexandresousaroberto@gmail.com>
2025-05-10 12:36:17 -0300

    alteração na mensagem do zapp

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -794,13 +794,13 @@
         </a>
     </div>
 
-    <a href="https://wa.me/5535999810371?text=Se%20tiver%20alguma%20d%C3%BAvida%2C%20entre%20em%20contato."
+    <a href="https://wa.me/5535999810371?text=Olá"
         target="_blank"
         style="position: fixed; bottom: 1rem; left: 1rem; padding: 0.75rem 1rem; background-color: #25D366; color: white; border-radius: 9999px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); display: flex; align-items: center; gap: 0.5rem; z-index: 50; text-decoration: none; transition: background-color 0.3s;"
         onmouseover="this.style.backgroundColor='#1ebe5d'"
         onmouseout="this.style.backgroundColor='#25D366'">
         <img src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/whatsapp.svg" alt="WhatsApp" style="width: 1.25rem; height: 1.25rem;">
-        Fale conosco
+        Fale conosco'
     </a>
     <div class="flex justify-center items-center gap-4 mt-6 mb-6 text-sm text-center">
         <a href="{{ route('terms.index') }}" class="text-gray-600 hover:underline">
