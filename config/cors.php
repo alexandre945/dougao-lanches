b398cec480172589cda2f7f4e5d7bf519b9740ab
Alexandre Sousa <alexandresousaroberto@gmail.com>
2025-08-06 21:29:14 -0300

    testando rota para pagar dados da api

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'products-api-vercel'],
 
     'allowed_methods' => ['*'],
 
