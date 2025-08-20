0f7d1546345016ef7b6d1a8106ccd12333bdc3db
Alexandre Sousa <alexandresousaroberto@gmail.com>
2025-08-20 20:22:32 -0300

    novas alteracões no layouts de atualizações

--- resources/views/products/showBomboniere.blade.php
+++ resources/views/products/showBomboniere.blade.php
@@ -41,7 +41,7 @@
             <p class="text-sm text-gray-500">Aqui você pode excluir, atualizar ou desativar um produto</p>
         </div>
 
-        <div class="flex justify-center space-x-2">
+        <div class="flex flex-wrap justify-center space-x-2">
                 <a href="{{ route('create.product') }}">
                     <div class="bg-gradient-to-r from-emerald-400 to-slate-400 border-l-4 border-blue-500 border-t-2 px-3 py-1 mt-2 ml-2 rounded text-xs">
                         LANCHES
@@ -70,25 +70,27 @@
                     <div class="bg-white rounded shadow-md mb-4 ">
                             <div class="p-2 border-b-2  border-gray-200">
                 
-                               <h1 class="text-start text-2xl p-2">{{$products->name}}</h1>
+                               <h1 class="text-start text-lg p-2">{{$products->name}}</h1>
                             
                             <div class=" flex flex-row justify-between gap-2 text-sm items-center p-2">
-                               <p class="text-sm">ATUALIZAR</p>
-                              <button class="btn btn-success" data-bs-toggle="modal"
-                                data-bs-target="#firstModal{{$products->id}}">
-                                <i class="fa-regular fa-pen-to-square text-sm "></i>
-                              </button>
-
+                                <div class="">
+                                  <button class="btn btn-success" data-bs-toggle="modal"
+                                    data-bs-target="#firstModal{{$products->id}}">
+                                    <i class="fa-regular fa-pen-to-square text-sm "></i>
+                                  </button>
+                                  <p class="text-sm mt-2">ATUALIZAR</p>
+                                </div>
                                   <div class="pr-2 produto flex flex-row lg:flex-row  w-full" w-full>
-                                    <p class="text-sm">EXCLUIR</p>
-                                    <form action="{{ route('delete.product',$products->id)}}" method="post">
-                                        @method('DELETE' )
-                                        @csrf
-                                        <button type="submit" class="" onclick="preventDefoult">
-                                          <i class="icon fa-sharp fa-solid fa-trash text-red-500"></i>
-                                        </button>
-                                    </form>
-
+                                      <div class="">
+                                        <form action="{{ route('delete.product',$products->id)}}" method="post">
+                                            @method('DELETE' )
+                                            @csrf
+                                            <button type="submit" class="" onclick="preventDefoult">
+                                              <i class="icon fa-sharp fa-solid fa-trash text-red-500"></i>
+                                            </button>
+                                        </form>
+                                        <p class="text-sm">EXCLUIR</p>
+                                      </div>
                                     <form action="{{ route('product.update',$products->id)}}" method="POST" >
                                         @csrf
                                     <div class="flex">
--- resources/views/products/showCombo.blade.php
+++ resources/views/products/showCombo.blade.php
@@ -40,7 +40,7 @@
             <h1 class="text-sm font-bold text-gray-700">ÁREA ADMINISTRATIVA</h1>
             <p class="text-sm text-gray-500">Aqui você pode excluir, atualizar ou desativar um Combo</p>
         </div>
-          <div class="flex justify-center space-x-2">
+          <div class="flex flex-wrap overflow-x-auto justify-center ">
                 <a href="{{ route('create.product') }}">
                     <div class="bg-gradient-to-r from-emerald-400 to-slate-400 border-l-4 border-blue-500 border-t-2 px-3 py-1 mt-2 ml-2 rounded text-xs">
                         LANCHES
@@ -71,24 +71,30 @@
                         <div class="bg-white rounded shadow-md mb-4 ">
                             <div class="p-2 border-b-2  border-gray-200">
                 
-                               <h1 class="text-start text-2xl p-2">{{$products->name}}</h1>
+                               <h1 class="text-start p-2 text-lg">{{$products->name}}</h1>
                             
                             <div class=" flex flex-row justify-between gap-2 text-sm items-center p-2">
-                               <p class="text-sm">ATUALIZAR</p>
-                              <button class="btn btn-success" data-bs-toggle="modal"
-                                data-bs-target="#firstModal{{$products->id}}">
-                                <i class="fa-regular fa-pen-to-square text-sm "></i>
-                              </button>
+                               <div class=""> 
+                                    <button class="btn btn-success" data-bs-toggle="modal"
+                                      data-bs-target="#firstModal{{$products->id}}">
+                                      <i class="fa-regular fa-pen-to-square text-sm "></i>
+                                    </button>
+                                   <p class="text-xs">ATUALIZAR</p>
+                                </div>
 
                                   <div class="pr-2 produto flex flex-row lg:flex-row  w-full" w-full>
-                                    <p class="text-sm">EXCLUIR</p>
-                                    <form action="{{ route('delete.product',$products->id)}}" method="post">
-                                        @method('DELETE' )
-                                        @csrf
-                                        <button type="submit" class="" onclick="preventDefoult">
-                                          <i class="icon fa-sharp fa-solid fa-trash text-red-500"></i>
-                                        </button>
-                                    </form>
+                                    
+                                   <div class="">
+                                      <form action="{{ route('delete.product',$products->id)}}" method="post">
+                                          @method('DELETE' )
+                                          @csrf
+                                          <button type="submit" class="" onclick="preventDefoult">
+                                            <i class="icon fa-sharp fa-solid fa-trash text-red-500"></i>
+                                          </button>
+                                      </form>
+                                       <p class="text-xs">EXCLUIR</p>
+                                    </div>
+                                   
 
                                     <form action="{{ route('product.update',$products->id)}}" method="POST" >
                                         @csrf
